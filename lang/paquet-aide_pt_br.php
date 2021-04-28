0cec7bad23830afdcef1a59adfcae29be517e5a8
Ricardo Porto <rporto@calepino.com>
2021-04-29 01:27:01 +0200

    [Salvatore] [source:lang/ paquet-aide] Export depuis https://trad.spip.net de la langue pt_br [Salvatore] [source:lang/ paquet-aide] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-aide_pt_br.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-aide?lang_cible=pt_br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'Este plugin permite incluir ajuda contextual no SPIP, acessível por um ícone. Esta ajuda pode ser também extendida aos plugins.',
+	'aide_nom' => 'Ajuda do SPIP',
+	'aide_slogan' => 'Ajuda online do SPIP'
+);
