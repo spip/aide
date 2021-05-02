8eac6a372d15e40beb436542557c9b26ca734853
George <george@middleeastwatch.net>
2021-05-03 01:27:01 +0200

    [Salvatore] [source:lang/ paquet-aide] Export depuis https://trad.spip.net de la langue ar [Salvatore] [source:lang/ paquet-aide] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ paquet-aide] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-aide_ar.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-aide?lang_cible=ar
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'يتيح هذا الملحق إدخال تعليمات فورية على SPIP يُدلّ اليها برمز معين. يمكن تعميم هذه التعليمات على ملاحق أخرى.',
+	'aide_nom' => 'تعليمات »[ّ[',
+	'aide_slogan' => 'تعليمات SPIP الفورية'
+);
---
+++ lang/paquet-aide_en.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-aide?lang_cible=en
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'This plug-in includes a contextual help in SPIP which is indicated by an icon. This help can be extended to other plug-ins.',
+	'aide_nom' => 'SPIP help',
+	'aide_slogan' => 'SPIP online help'
+);
