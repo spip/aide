072dd78d3f2f992b8c8bfae57eee983b5d3a31dd
Jack31 <jacques@jack31.org>
2021-05-25 18:28:54 +0200

    aide en espagnol

---
+++ lang/aide_es.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    'raccourcis' => 'Atajos',
+    'raccourcis_ancre' => 'Anclas con nombres',
+    'raccourcis_citation' => 'Citar un extracto',
+    'raccourcis_code' => 'Mostrar código informático',
+    'raccourcis_glossaire' => 'Glosario externo',
+    'raccourcis_lien' => 'Enlaces de hipertexto',
+    'raccourcis_liste' => 'Listas y enumeraciones',
+    'raccourcis_note' => 'Notas de pie de página',
+    'raccourcis_resume' => 'En resumen',
+    'raccourcis_simple' => 'Formato simple',
+    'raccourcis_tableau' => 'Tablas',
+);
