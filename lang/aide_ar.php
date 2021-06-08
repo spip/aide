aae6b8de4a66e6382bebda23aafbc6694ffb80ec
Webmaster91 <GSIC-Etudes_Dev@sdis91.fr>
2021-06-08 14:22:03 +0200

    Aide en arabe

---
+++ lang/aide_ar.php
@@ -0,0 +1,15 @@
+<?php
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    'raccourcis' => 'إختصارات الكتابة',
+    'raccourcis_ancre' => 'مراسي تحمل اسماء',
+    'raccourcis_citation' => 'ذكر مقتطفات',
+    'raccourcis_code' => 'عرض الرموز البرمجية',
+    'raccourcis_glossaire' => 'معجم خارجي',
+    'raccourcis_lien' => 'روابط هايبرتكست',
+    'raccourcis_liste' => 'اللوائح والترقيم',
+    'raccourcis_note' => 'الحواشي',
+    'raccourcis_resume' => 'ياختصار',
+    'raccourcis_simple' => 'تصميم مبسط',
+    'raccourcis_tableau' => 'الجداول',
+);
