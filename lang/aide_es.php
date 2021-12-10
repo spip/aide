f2f2368d93f445bfff862df388050d48cc1dc378
Salvatore <salvatore@rezo.net>
2021-12-10 01:27:01 +0100

    [Salvatore] [source:lang/ aide] Export depuis https://trad.spip.net de la langue ar [Salvatore] [source:lang/ aide] Export depuis https://trad.spip.net de la langue en [Salvatore] [source:lang/ aide] Export depuis https://trad.spip.net de la langue es [Salvatore] [source:lang/ aide] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ aide] Mise a jour du bilan depuis https://trad.spip.net

--- lang/aide_es.php
+++ lang/aide_es.php
@@ -1,15 +1,24 @@
 <?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/aide-aide?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-    'raccourcis' => 'Atajos',
-    'raccourcis_ancre' => 'Anclas con nombres',
-    'raccourcis_citation' => 'Citar un extracto',
-    'raccourcis_code' => 'Mostrar código informático',
-    'raccourcis_glossaire' => 'Glosario externo',
-    'raccourcis_lien' => 'Enlaces de hipertexto',
-    'raccourcis_liste' => 'Listas y enumeraciones',
-    'raccourcis_note' => 'Notas de pie de página',
-    'raccourcis_resume' => 'En resumen',
-    'raccourcis_simple' => 'Formato simple',
-    'raccourcis_tableau' => 'Tablas',
+
+	// R
+	'raccourcis' => 'Atajos',
+	'raccourcis_ancre' => 'Anclas con nombres',
+	'raccourcis_citation' => 'Citar un extracto',
+	'raccourcis_code' => 'Mostrar código informático',
+	'raccourcis_glossaire' => 'Glosario externo',
+	'raccourcis_lien' => 'Enlaces de hipertexto',
+	'raccourcis_liste' => 'Listas y enumeraciones',
+	'raccourcis_note' => 'Notas de pie de página',
+	'raccourcis_resume' => 'En resumen',
+	'raccourcis_simple' => 'Formato simple',
+	'raccourcis_tableau' => 'Tablas'
 );
