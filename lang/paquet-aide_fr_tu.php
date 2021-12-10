33cc88a586e868dc45ffe27a9ed3fcac4d702e06
RealET <real3t@gmail.com>
2021-12-10 10:20:02 +0100

    [Salvatore] [source:lang/ paquet-aide] Export depuis https://trad.spip.net de la langue fr_tu [Salvatore] [source:lang/ paquet-aide] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-aide_fr_tu.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-aide?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'Ce plugin permet d’inclure dans SPIP une aide contextuelle repérée par une icone. Cette aide peut-être étendue aussi aux plugins.',
+	'aide_nom' => 'Aide SPIP',
+	'aide_slogan' => 'Aide en ligne de SPIP'
+);
