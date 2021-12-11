a42d696d681d932af241684e78b3458b7a567970
RealET <real3t@gmail.com>
2021-12-11 01:27:02 +0100

    [Salvatore] [source:lang/ aide] Export depuis https://trad.spip.net de la langue fr_tu

---
+++ lang/aide_fr_tu.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/aide-aide?lang_cible=fr_tu
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'raccourcis' => 'Raccourcis typographiques',
+	'raccourcis_ancre' => 'Ancres nommées',
+	'raccourcis_citation' => 'Citations',
+	'raccourcis_code' => 'Code informatique',
+	'raccourcis_glossaire' => 'Glossaire externe',
+	'raccourcis_lien' => 'Liens hypertextes',
+	'raccourcis_liste' => 'Listes et énumérations',
+	'raccourcis_note' => 'Notes de bas de page',
+	'raccourcis_resume' => 'En résumé',
+	'raccourcis_simple' => 'Mise en forme simple',
+	'raccourcis_tableau' => 'Tableaux'
+);
