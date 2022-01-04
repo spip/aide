e8d945ff9aa5af27f33d229be21ceede48a2f8a9
Jorge Paulo Malveira Pimentel Dos Santos <jpmpds@yahoo.fr>
2022-01-04 01:27:01 +0100

    [Salvatore] [source:lang/ aide] Export depuis https://trad.spip.net de la langue pt [Salvatore] [source:lang/ aide] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/aide_pt.php
@@ -0,0 +1,24 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/aide-aide?lang_cible=pt
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'raccourcis' => 'Atalhos tipográficos',
+	'raccourcis_ancre' => 'Âncoras nomeadas',
+	'raccourcis_citation' => 'Citações',
+	'raccourcis_code' => 'Código de computação', # RELIRE
+	'raccourcis_glossaire' => 'Glossário externo',
+	'raccourcis_lien' => 'Ligação (link) de hipertexto',
+	'raccourcis_liste' => 'Listas e enumerações',
+	'raccourcis_note' => 'Notas de rodapé',
+	'raccourcis_resume' => 'Resumindo',
+	'raccourcis_simple' => 'Formatação simples',
+	'raccourcis_tableau' => 'Quadros' # RELIRE
+);
