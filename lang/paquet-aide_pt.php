3e4004a0647519d9bc1e6453cb83af981993a981
Jorge Paulo Malveira Pimentel Dos Santos <jpmpds@yahoo.fr>
2022-01-04 01:27:03 +0100

    [Salvatore] [source:lang/ paquet-aide] Export depuis https://trad.spip.net de la langue pt [Salvatore] [source:lang/ paquet-aide] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-aide_pt.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-aide?lang_cible=pt
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'Este plugin permite incluir no SPIP uma ajuda contextual identificada por um ícone. Esta ajuda também pode ser estendida aos plugins.',
+	'aide_nom' => 'Ajuda SPIP',
+	'aide_slogan' => 'Ajuda online do SPIP'
+);
