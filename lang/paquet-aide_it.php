79c42cf88f8838ba9628058b381ee34a3ca2b9d6
Alberto <alm82@live.it>
2022-02-04 01:27:12 +0100

    [Salvatore] [source:lang/ paquet-aide] Export depuis https://trad.spip.net de la langue it [Salvatore] [source:lang/ paquet-aide] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-aide_it.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-aide?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'Questo plugin consente a SPIP di includere una guida contestuale contrassegnata da un’icona. Questo aiuto può essere esteso anche ai plugin.',
+	'aide_nom' => 'Aiuto SPIP',
+	'aide_slogan' => 'Guida in linea di SPIP'
+);
