471219386fbeec9b7d4f7f359f67142eb7dca11d
Alberto <alm82@live.it>
2022-02-04 01:27:10 +0100

    [Salvatore] [source:lang/ aide] Export depuis https://trad.spip.net de la langue it [Salvatore] [source:lang/ aide] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/aide_it.php
@@ -0,0 +1,21 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/aide-aide?lang_cible=it
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'raccourcis' => 'Scorciatoie tipografiche',
+	'raccourcis_citation' => 'Citazioni',
+	'raccourcis_code' => 'Codice informatico',
+	'raccourcis_glossaire' => 'Glossario esterno',
+	'raccourcis_lien' => 'Collegamenti ipertestuali',
+	'raccourcis_note' => 'Note a piè di pagina',
+	'raccourcis_resume' => 'In sintesi',
+	'raccourcis_simple' => 'Formattazione semplice'
+);
