7d4652dc39109f5f1885801c219d9b8cb8aada49
Torsten Willmann <willmann@netzsachen.de>
2022-03-02 01:27:27 +0100

    [Salvatore] [source:lang/ paquet-aide] Export depuis https://trad.spip.net de la langue de [Salvatore] [source:lang/ paquet-aide] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-aide_de.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-aide?lang_cible=de
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'Dieses Plugin ermöglicht es, in SPIP eine kontextsensitive Hilfe einzufügen, die durch ein Icon gekennzeichnet ist. Diese Hilfe kann auch auf Plugins ausgeweitet werden. ',
+	'aide_nom' => 'SPIP Hilfe',
+	'aide_slogan' => 'SPIP Online-Hilfe'
+);
