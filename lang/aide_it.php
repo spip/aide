9bc2e3fc115e81ce0400ca67bc35647e7d0cffc0
Alberto <alm82@live.it>
2022-05-01 01:27:19 +0200

    [Salvatore] [source:lang/ aide] Export depuis https://trad.spip.net de la langue it [Salvatore] [source:lang/ aide] Mise a jour du bilan depuis https://trad.spip.net

--- lang/aide_it.php
+++ lang/aide_it.php
@@ -11,11 +11,14 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// R
 	'raccourcis' => 'Scorciatoie tipografiche',
+	'raccourcis_ancre' => 'Nomi delle scorciatoie',
 	'raccourcis_citation' => 'Citazioni',
 	'raccourcis_code' => 'Codice informatico',
 	'raccourcis_glossaire' => 'Glossario esterno',
 	'raccourcis_lien' => 'Collegamenti ipertestuali',
+	'raccourcis_liste' => 'Elenchi ed enumerazioni',
 	'raccourcis_note' => 'Note a piè di pagina',
 	'raccourcis_resume' => 'In sintesi',
-	'raccourcis_simple' => 'Formattazione semplice'
+	'raccourcis_simple' => 'Formattazione semplice',
+	'raccourcis_tableau' => 'Tabelle'
 );
