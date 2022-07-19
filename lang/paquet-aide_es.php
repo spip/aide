703d6a394a070e4682fda5c73b985bb292c0572e
Matthieu Marcillaud <marcimat@rezo.net>
2022-07-19 17:11:17 +0200

    i18n: Report chaines de langues (Salvatore)

---
+++ lang/paquet-aide_es.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-aide?lang_cible=es
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'aide_description' => 'Este plugin permite que SPIP incluya ayuda contextual marcada con un icono. Esta ayuda también se puede extender a los plugins.',
+	'aide_nom' => 'Ayuda SPIP',
+	'aide_slogan' => 'Ayuda en línea de SPIP'
+);
